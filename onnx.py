2f0deb4eb96702ee789439a8b0ac54f4fe8e5af7
Shreyas.H.C <37148729+Shreyas2409@users.noreply.github.com>
2020-06-05 14:34:48 +0530

    Update onnx.py

--- onnx.py
+++ onnx.py
@@ -3,4 +3,4 @@ from modules import onnx_convert_api
 HD5_MODEL = './model/pnuemonia.h5'
 ONNX_TARGET = './model/pnuemonia.onnx'
 
-onnx_convert_api(HD5_MODEL, ONNX_TARGET, './samples')
+onnx_convert_api(HD5_MODEL, ONNX_TARGET, './testcase')
